51b7bbe35ccfbb08c940fff4e223ec49e05cf160
Memder <morozovvlad24753@gmail.com>
2026-05-06 01:13:36 +0300

    Начало структуризации

---
+++ handlers/register.py
@@ -0,0 +1,10 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes
+
+fac_keyboard = [
+    [
+        InlineKeyboardButton('ИПИТ', callback_data=1)
+    ]
+]
+
+
